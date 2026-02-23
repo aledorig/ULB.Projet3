0afbd42b268aaf382c6ffb9a6a62115cd8ebcb7d
UnderNowhere <115153570+UnderNowhere@users.noreply.github.com>
2026-02-23 23:36:45 +0100

    Flat terrain + coast detection

--- scripts/world/gen/river/river_generator.gd
+++ scripts/world/gen/river/river_generator.gd
@@ -2,9 +2,15 @@ class_name RiverGenerator
 extends RefCounted 
 
 var terrain: TerrainGenerator
+var terrain_cache: Dictionary[Vector2, Vector2] = {}
+var coast_cells_cache: Dictionary[Vector2, PackedVector2Array] = {}
+var flat_cells_cache: Dictionary[Vector2, PackedVector2Array] = {}
 
-func _init(p_terrain:TerrainGenerator) -> void:
+func _init(p_terrain: TerrainGenerator) -> void:
 	terrain = p_terrain
+	terrain_cache = {}
+	coast_cells_cache = {}
+	flat_cells_cache = {}
 	
 func is_best_among_neighboors(candidate: Vector3, sources_candidates : Array[Vector3]) -> bool:#
 	# Check among the candidates the neighboors first
@@ -22,7 +28,7 @@ func is_best_among_neighboors(candidate: Vector3, sources_candidates : Array[Vec
 			   -terrain.get_gradient(source.x, source.z):
 				return false
 	return true
-	
+
 func find_source(center: Vector2, area_size: float) -> Array[Vector3]:
 	var step := 80.0
 	var half := area_size / 2.0
@@ -67,3 +73,92 @@ func build_river_controls_points(source:Vector3) -> PackedVector3Array:
 		pos += slope_dir * step_dist
 		
 	return path
+
+func _cached_cell(pos: Vector2) -> Vector2:
+	# Cache hit :-)
+	if terrain_cache.has(pos):
+		return terrain_cache[pos]
+	
+	# Cache miss, ohh come on :-(
+	var height := terrain.get_height(pos.x, pos.y)
+	var grad := -terrain.get_gradient(pos.x, pos.y)
+	var slope_mag := grad.length() 
+
+	var sample := Vector2(height, slope_mag)
+	terrain_cache[pos] = sample
+	return sample
+
+func load_grid(center: Vector2, area_size: int) -> void:
+	var step := 64.0
+	var sea_level = 0.0
+	var mountains_level = 130.0
+	
+	# TODO check the tresholds values...
+	var slope_threshold := 40.0  
+	var delta_height_threshold := 40.0 
+
+	var half := float(area_size) * 0.5
+	var origin := center - Vector2(half, half)
+
+	var cells_x := int(floor(float(area_size) / step))
+	var cells_z := int(floor(float(area_size) / step))
+
+	var coast_cells := PackedVector2Array()
+	var flat_cells := PackedVector2Array()
+
+	for gx in range(cells_x):
+		for gz in range(cells_z):
+			var x0 := origin.x + gx * step
+			var z0 := origin.y + gz * step
+
+			# Cell corners
+			var bl := Vector2(x0, z0)               # BL
+			var br := Vector2(x0 + step, z0)        # BR
+			var ul := Vector2(x0, z0 + step)        # UL
+			var ur := Vector2(x0 + step, z0 + step) # UR
+
+			# Cell center
+			var cc := Vector2(x0 + step * 0.5, z0 + step * 0.5)
+			
+			# V for vector ...
+			var v_bl := _cached_cell(bl)
+			var v_br := _cached_cell(br)
+			var v_ul := _cached_cell(ul)
+			var v_ur := _cached_cell(ur)
+			var v_cc  := _cached_cell(cc)
+
+			# Heights
+			var hbl := v_bl.x
+			var hbr := v_br.x
+			var hul := v_ul.x
+			var hur := v_ur.x
+			var hcc := v_cc.x
+
+			var sbl := v_bl.y
+			var sbr := v_br.y
+			var sul := v_ul.y
+			var sur := v_ur.y
+			var scc := v_cc.y
+			
+			var min_h = min(min(hbl, hbr), min(min(hul, hur), hcc))
+			var max_h = max(max(hbl, hbr), max(max(hul, hur), hcc))
+			if hcc > 0.0 and min_h <= 0.0 and max_h >= 0.0:
+				coast_cells.append(cc)
+
+			var average_slope := (sbl + sbr + sul + sur + scc) / 5.0
+			var delta_height = max_h - min_h
+
+			if hcc > sea_level and hcc <= mountains_level \
+			and average_slope <= slope_threshold \
+			and delta_height <= delta_height_threshold:
+				flat_cells.append(cc)
+
+	coast_cells_cache[center] = coast_cells
+	flat_cells_cache[center] = flat_cells
+	
+func get_flat_cells(center: Vector2) -> PackedVector2Array:
+	return flat_cells_cache[center]
+	
+func get_coast_cells(center: Vector2) -> PackedVector2Array:
+	return coast_cells_cache[center]
+	
--- scripts/world/gen/river/river_visualizer.gd
+++ scripts/world/gen/river/river_visualizer.gd
@@ -1,23 +1,52 @@
 class_name RiverVisualizer
 extends Node3D
 
+const CELL_SIZE := 64.0
+const CELL_Y_OFFSET := 1.5 # slightly above terrain to avoid z-fighting
+
 var mesh: ImmediateMesh
 var mesh_instance: MeshInstance3D
 var material: StandardMaterial3D
 
+var coast: ImmediateMesh
+var coast_instance: MeshInstance3D
+var coast_material: StandardMaterial3D
+
+var flat: ImmediateMesh
+var flat_instance: MeshInstance3D
+var flat_material: StandardMaterial3D
 
 func _ready() -> void:
 	mesh = ImmediateMesh.new()
-
+	coast = ImmediateMesh.new()
+	flat = ImmediateMesh.new()
+	
 	mesh_instance = MeshInstance3D.new()
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
 	add_child(mesh_instance)
+	
+	coast_instance = MeshInstance3D.new()
+	coast_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	add_child(coast_instance)
+
+	flat_instance = MeshInstance3D.new()
+	flat_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	add_child(flat_instance)
 
 	material = StandardMaterial3D.new()
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 	material.albedo_color = Color(0.2, 0.5, 1.0)
 	material.no_depth_test = true
 
+	coast_material = StandardMaterial3D.new()
+	coast_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	coast_material.albedo_color = Color(0.857, 0.338, 0.194, 1.0)
+	coast_material.no_depth_test = true
+
+	flat_material = StandardMaterial3D.new()
+	flat_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	flat_material.albedo_color = Color(0.337, 0.91, 0.333, 1.0)
+	flat_material.no_depth_test = true
 
 func draw_candidates(candidates: Array[Vector3]) -> void:
 	mesh.clear_surfaces()
@@ -38,6 +67,53 @@ func draw_candidates(candidates: Array[Vector3]) -> void:
 	mesh_instance.mesh = mesh
 	print("[RIVER] Drawing %d candidates" % candidates.size())
 
+func _build_square(imesh: ImmediateMesh, center: Vector2, y: float, size: float) -> void:
+	var half := size * 0.5
+
+	var bl := Vector3(center.x - half, y, center.y - half)
+	var br := Vector3(center.x + half, y, center.y - half)
+	var ur := Vector3(center.x + half, y, center.y + half)
+	var ul := Vector3(center.x - half, y, center.y + half)
+
+	imesh.surface_add_vertex(bl)
+	imesh.surface_add_vertex(br)
+	imesh.surface_add_vertex(br)
+	imesh.surface_add_vertex(ur)
+	imesh.surface_add_vertex(ur)
+	imesh.surface_add_vertex(ul)
+	imesh.surface_add_vertex(ul)
+	imesh.surface_add_vertex(bl)
+
+func draw_flat_cells(flats: PackedVector2Array) -> void:
+	flat.clear_surfaces()
+
+	if flats.is_empty():
+		return
+
+	# Wireframe squares (same dimension as CELL_SIZE)
+	flat.surface_begin(Mesh.PRIMITIVE_LINES, flat_material)
+	for c in flats:
+		_build_square(flat, c, CELL_Y_OFFSET, CELL_SIZE)
+	flat.surface_end()
+
+	flat_instance.mesh = flat
+	print("[RIVER] Drawing %d flat cells" % flats.size())
+
+
+func draw_coast_cells(coasts: PackedVector2Array) -> void:
+	coast.clear_surfaces()
+
+	if coasts.is_empty():
+		return
+
+	coast.surface_begin(Mesh.PRIMITIVE_LINES, coast_material)
+	for c in coasts:
+		_build_square(coast, c, CELL_Y_OFFSET + 0.2, CELL_SIZE)
+	coast.surface_end()
+
+	coast_instance.mesh = coast
+	print("[RIVER] Drawing %d coast cells" % coasts.size())
+
 func draw_rivers(rivers: Array[PackedVector3Array]) -> void:
 	# using bezier curve to build the path...
 	
@@ -52,7 +128,6 @@ func draw_rivers(rivers: Array[PackedVector3Array]) -> void:
 		mesh.surface_end()
 
 	mesh_instance.mesh = mesh
-
 
 func clear() -> void:
 	mesh.clear_surfaces()
